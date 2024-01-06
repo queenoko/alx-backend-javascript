8ca65d9de17823927c88a38aa6e6648255efaa28
queenoko <quimaudongwo@gmail.com>
2024-01-06 21:45:42 +0100

    current year param

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,13 @@
+/* current yr param */
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+}
