77cdfdeeb474805e3d442eb962ea10c0a66beafa
queenoko <quimaudongwo@gmail.com>
2024-01-07 17:26:29 +0100

    building method

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+/*  building sky */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
