b390510ea53fd7823cd1568d9ec57c3b8d6c843c
queenoko <quimaudongwo@gmail.com>
2024-01-07 16:45:31 +0100

    currency getter

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+/* currenccy */
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    this._name = n;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(c) {
+    this._name = c;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
