705b7b0d25fac19875c61d978fe85c3672db2e45
queenoko <quimaudongwo@gmail.com>
2024-01-07 17:34:43 +0100

    airport function

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,27 @@
+/* airport class */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}
