13b251155b20aa2c0590df47986020fdadf0ed39
queenoko <quimaudongwo@gmail.com>
2024-02-29 11:39:08 +0000

    Server side

---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,16 @@
+// This script is the server side
+const express = require('express');
+const router = require('./routes/index');
+
+const app = express();
+const port = 1245;
+
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
+module.exports = app;
