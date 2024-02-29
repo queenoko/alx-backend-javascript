399ea0d4af7cb5eb5bab4eb2c9010d707372e220
queenoko <quimaudongwo@gmail.com>
2024-02-29 13:38:45 +0000

    nodejs http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+// Nodejs HTTP module
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
+
+module.exports = app;
