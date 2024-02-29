0659d31a1170dcde7787dd668eaf8b100381bfa4
queenoko <quimaudongwo@gmail.com>
2024-02-29 11:51:13 +0000

    Controller

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,8 @@
+// Controller function
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
